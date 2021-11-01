0a826ca9796cced7668a8294486cef04e5a69da4
Katherine Pitts <kvp06933@gmail.com>
2021-11-01 00:41:44 -0500

    Updated routing, IImageService injection locations

--- KatyDevBlog/Controllers/HomeController.cs
+++ KatyDevBlog/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using KatyDevBlog.Data;
 using KatyDevBlog.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -16,17 +17,35 @@ namespace KatyDevBlog.Controllers
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly ILogger<HomeController> _logger;
+        private readonly IEmailSender _emailService;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IEmailSender emailService)
         {
             _logger = logger;
             _dbContext = context;
+            _emailService = emailService;
         }
 
         public async Task<IActionResult> Index()
         {
             //Show all blogs
             return View(await _dbContext.Blogs.ToListAsync()); //ToListAsync lets us enumerate through a list ansycronously
+        }
+        public IActionResult ContactMe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ContactMe(string name, string email, string phone, string message)
+        {
+            var subject = $"{name} has reached out to you from KatyDev Blog.";
+            //TODO: Add code to call the Email Service
+
+            var body = $"{message}.<br/><br/>{name} can be reached at {phone} if follow up is required.";
+            await _emailService.SendEmailAsync(email, subject, body);
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
---
+++ KatyDevBlog/Services/BasicEmailService.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KatyDevBlog.Services
+{
+    public class BasicEmailService : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public BasicEmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var newEmail = new MimeMessage();
+
+            //I need to talk to appsettings.json
+            var emailAddress = _configuration["SmtpSettings:Email"];
+            newEmail.Sender = MailboxAddress.Parse(emailAddress);
+            newEmail.To.Add(MailboxAddress.Parse(emailAddress));
+            newEmail.Subject = subject;
+
+            var body = new BodyBuilder();
+            body.HtmlBody = htmlMessage;
+            newEmail.Body = body.ToMessageBody();
+
+            //Configure the SMTP server to send the newEmail
+            using SmtpClient smtpClient = new();
+            var host = _configuration["SmtpSettings:Host"];
+            var port = Convert.ToInt32(_configuration["SmtpSettings:Port"]);
+            smtpClient.Connect(host, port, MailKit.Security.SecureSocketOptions.StartTls);
+            smtpClient.Authenticate(emailAddress, _configuration["SmtpSettings:Password"]);
+            await smtpClient.SendAsync(newEmail);
+
+            smtpClient.Disconnect(true);
+        }
+    }
+}
